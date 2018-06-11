a6e978b2041cdb68755e07d152c788ce05b7f059
Dave Boyce <daveb@501commons.org>
2018-06-11 13:09:20 -0700

    update

--- Clients/OPPCO/Queries/query-Household_Member_Data.sql
+++ Clients/OPPCO/Queries/query-Household_Member_Data.sql
@@ -1,7 +1,7 @@
 select 
     "name_last", "name_first", "name_mi"
     ,"_kf_hh_data_id", "_kp_hh_member_data_id"
-    ,"Program"
+    ,"Program", "ssn_conversion"
     ,"dob", "gender", "race", "ethnicity", "disabled", "veteran", "education", "insur_health"
 from
     "Household Member Data"
