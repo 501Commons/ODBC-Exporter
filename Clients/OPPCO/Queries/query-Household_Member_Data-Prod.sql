ac5278518928f78c7c42db733c4789aec42719fa
Dave Boyce <daveb@501commons.org>
2018-06-15 12:37:14 -0700

    add age

--- Clients/OPPCO/Queries/query-Household_Member_Data-Prod.sql
+++ Clients/OPPCO/Queries/query-Household_Member_Data-Prod.sql
@@ -2,6 +2,6 @@ select
     "name_last", "name_first", "name_mi"
     ,"_kf_hh_data_id", "_kp_hh_member_data_id"
     ,"Program", "ssn_conversion"
-    ,"dob", "gender", "race", "ethnicity", "disabled", "veteran", "education", "insur_health"
+    ,"dob", "age", "gender", "race", "ethnicity", "disabled", "veteran", "education", "insur_health"
 from
     "Household Member Data"
