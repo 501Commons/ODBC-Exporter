7ca828862338ab5c9f0597fe3c9992769fbcfdde
Dave Boyce <daveb@501commons.org>
2018-09-07 00:26:48 -0700

    childplus

---
+++ Clients/OPPCO/Queries/query-ChildPlus-Contact-Prod.sql
@@ -0,0 +1,57 @@
+SELECT TOP 100
+    Person.ChildPlusID
+    , Person.PersonID
+	, FamilyMembership.FamilyID
+	, Person.FirstName
+	, Person.MiddleName
+	, Person.LastName
+	, Person.NameSuffix
+	, Person.PreferredName
+	, Person.PreviousName
+    , Person.Birthday
+	, vPerson.Age
+	, vPerson.GenderDescription
+	, vPerson.RaceDescription
+	, vPerson.PrimaryLanguageDescription
+	, vPerson.SecondaryLanguageDescription
+	, vPerson.ThirdLanguageDescription
+    ,CASE
+		WHEN vPerson.IsHispanic = 1 THEN 'Yes'
+		WHEN vPerson.IsHispanic = 0 THEN 'No'
+		WHEN vPerson.IsHispanic >= 2 THEN 'Unknown/Not reported'
+ 	END AS EthnicityHispanicLatino    
+	, Person.Email1
+	, RIGHT(vPerson.SSN, 4) as SSNShort
+    , Person.MailAddress1, Person.MailAddress2, Person.MailCity, Person.MailState, Person.MailZip
+    , Person.PhysicalAddress1, Person.PhysicalAddress2, Person.PhysicalCity, Person.PhysicalState, Person.PhysicalZip
+    , PrimaryPhone.PhoneType, PrimaryPhone.PhoneNumber, PrimaryPhone.Extension, PrimaryPhone.FullDescription
+    , SecondaryPhone.PhoneType, SecondaryPhone.PhoneNumber, SecondaryPhone.Extension, SecondaryPhone.FullDescription
+	,(SELECT
+		COUNT(*)
+		FROM DisabilityConcern DC
+		JOIN DisabilityConcernActivity DCA
+			ON DCA.DisabilityConcernID = DC.DisabilityConcernID
+		WHERE Vperson.PersonID = DC.PersonID
+			AND DCA.DisabilityConcernActivityTypeID = 'CDCD74A7-A992-446C-81BD-149382E92BD4'
+			AND (DC.ClosedDate IS NULL
+			OR DC.ClosedDate > GETDATE())) as Disabled
+	, CodeEducation.Description as Education
+	, CodeEmployment.Description as Employment
+FROM Vperson
+INNER JOIN Person
+	   ON Person.PersonID = vPerson.PersonID
+INNER JOIN FamilyMembership
+       ON FamilyMembership.PersonID = vPerson.PersonID
+INNER JOIN FamilyMember
+	ON FamilyMember.PersonID = Vperson.PersonID
+LEFT JOIN Code CodeEducation
+	ON CodeEducation.CodeID = FamilyMember.EducationLevelCodeID
+LEFT JOIN Code CodeEmployment
+	ON CodeEmployment.CodeID = FamilyMember.EmploymentStatusCodeID
+LEFT JOIN vPersonPhone as PrimaryPhone
+	ON PrimaryPhone.PersonID = vPerson.PersonID AND
+		PrimaryPhone.PhoneRank <= 1
+LEFT JOIN vPersonPhone as SecondaryPhone
+	ON SecondaryPhone.PersonID = vPerson.PersonID AND
+		SecondaryPhone.PhoneRank = 2
+WHERE FamilyMembership.FamilyID = '73a0ac56-6a33-480a-ae93-5bf444d18ef2'		
---
+++ Clients/OPPCO/Queries/query-ChildPlus-ServiceHousehold-Prod.sql
@@ -0,0 +1,114 @@
+SELECT TOP 100
+	   ProgramTerm.ProgramTermName
+	   , ProgramTerm.BeginDate
+	   , ProgramTerm.EndDate
+	   , vCurrentStatusLoc.StatusDescription
+	   , vCurrentStatusLoc.TerminationReason
+	   , vCurrentStatusLoc.StartDate
+	   , vCurrentStatusLoc.EndDate
+	   , vCurrentStatusLoc.EntryDate
+	   , Family.FamilyID
+	   , vFamily.FamilyName
+	   , vFamily.NumberInFamily
+	   , vFamily.NumberInHousehold
+	   , Family.MailAddress1, Family.MailCity, Family.MailState, Family.MailZip, Family.MailingAddressStartDate
+	   , Family.PhysicalAddress1, Family.PhysicalCity, Family.PhysicalState, Family.PhysicalZip, Family.PhysicalAddressStartDate
+	   , PrimaryPhone.PhoneType, PrimaryPhone.PhoneNumber, PrimaryPhone.Extension, PrimaryPhone.FullDescription
+	   , SecondaryPhone.PhoneType, SecondaryPhone.PhoneNumber, SecondaryPhone.Extension, SecondaryPhone.FullDescription
+	   , vFamily.TANFStatusDescription
+	   , vFamily.SSIStatus
+	   ,CASE
+			WHEN ProgramParticipation.WICServicesReceived = 1 THEN 'Yes'
+			WHEN ProgramParticipation.WICServicesReceived = 0 THEN 'No'
+			WHEN ProgramParticipation.WICServicesReceived >= 2 THEN 'Unknown/Not reported'
+   	   END AS WIC    
+	   ,CASE
+			WHEN ProgramParticipation.SNAP = 1 THEN 'Yes'
+			WHEN ProgramParticipation.SNAP = 0 THEN 'No'
+			WHEN ProgramParticipation.SNAP >= 2 THEN 'Unknown/Not reported'
+   	   END AS SNAP    
+	   , vFamily.ParentTypeDescription
+	   ,CASE
+			WHEN ParentTypeCodeID = 'A49CC970-9B38-4DA5-A4F3-2E000C9CD8D8' AND
+				GenderCode = 'F' THEN 'Single Parent Female'
+			WHEN ParentTypeCodeID = 'A49CC970-9B38-4DA5-A4F3-2E000C9CD8D8' AND
+				GenderCode = 'M' THEN 'Single Parent Male'
+			WHEN ParentTypeCodeID = '5E3550B1-2452-4E6D-91CE-BDB30B9A5C86' THEN 'Two Parent Household'
+			WHEN Family.FamilyID IN (SELECT DISTINCT
+						FamilyID
+					FROM (SELECT DISTINCT
+							FamilyID
+						   ,COUNT(*) OVER (PARTITION BY familyID) AS NIF
+						FROM Family) S
+					WHERE NIF = 1) THEN 'Single Person'
+			WHEN 1 = 0 THEN 'Two Adults NO Children'
+			ELSE 'f. Other'
+		END AS FamilyType
+	   , vPerson.GenderDescription
+	   , vPerson.PersonName
+	   , vPerson.PersonID
+	   , vFamily.NumberInHousehold
+	   , Family.SocialSecurityIncome
+	   ,CASE
+			WHEN '88888888-0000-0000-0000-000000000002' IN (Family.Income1Descriptioncodeid, Family.Income2DescriptionCodeID, Family.Income3DescriptionCodeID, Family.Income4DescriptionCodeID, Family.Income5DescriptionCodeID) THEN 'Yes'
+			ELSE 'No'
+		END AS SocialSecurityIncomeCalculated
+	   ,CASE
+			WHEN '88888888-0000-0000-0000-000000000003' IN (Income1Descriptioncodeid, Income2DescriptionCodeID, Income3DescriptionCodeID, Income4DescriptionCodeID, Income5DescriptionCodeID) THEN 'Yes'
+			ELSE 'No'
+		END AS PensionCalculated
+	   ,CASE
+			WHEN '77777777-0000-0000-0000-000000000006' IN (Income1VerificationTypeCodeID, Income2VerificationTypeCodeID, Income3VerificationTypeCodeID, Income4VerificationTypeCodeID, Income5VerificationTypeCodeID) THEN 'Yes'
+			ELSE 'No'
+		END AS UnemploymentCalculated
+	   ,CASE
+			WHEN 'C29391B5-AF69-4668-9488-273B0A732E72' IN (Income1Descriptioncodeid, Income2DescriptionCodeID, Income3DescriptionCodeID, Income4DescriptionCodeID, Income5DescriptionCodeID) THEN 'Yes'
+			ELSE 'No'
+		END AS GeneralAssistanceCalculated
+	   ,CASE
+			WHEN
+				((Income1DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income2DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income3DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income4DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income5DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D') AND
+				(Income1DescriptionCodeID NOT IN ('00000000-0000-0000-0000-000000000000', '96143509-3ED4-47D4-BE99-32D94478832D') OR
+				Income2DescriptionCodeID NOT IN ('00000000-0000-0000-0000-000000000000', '96143509-3ED4-47D4-BE99-32D94478832D') OR
+				Income3DescriptionCodeID NOT IN ('00000000-0000-0000-0000-000000000000', '96143509-3ED4-47D4-BE99-32D94478832D') OR
+				Income4DescriptionCodeID NOT IN ('00000000-0000-0000-0000-000000000000', '96143509-3ED4-47D4-BE99-32D94478832D') OR
+				Income5DescriptionCodeID NOT IN ('00000000-0000-0000-0000-000000000000', '96143509-3ED4-47D4-BE99-32D94478832D')
+				)) THEN 'Yes'
+			ELSE 'No'
+		END AS EmploymentAndOtherCalculated
+	   ,CASE
+			WHEN ((Income1DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income2DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income3DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income4DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D' OR
+				Income5DescriptionCodeID = '96143509-3ED4-47D4-BE99-32D94478832D')) THEN 'Yes'
+			ELSE 'No'
+		END AS EmploymentOnlyCalculated
+	    ,ProgramParticipation.EligibilityIncome
+		,Family.MonthlyFamilyIncome
+	   
+FROM vFamily
+INNER JOIN Family
+	ON Family.FamilyID = vFamily.FamilyID
+INNER JOIN FamilyMembership
+	ON FamilyMembership.FamilyID = Family.familyID
+LEFT JOIN vPerson
+	ON vPerson.PersonID = FamilyMembership.PersonID
+LEFT JOIN ProgramParticipation
+	ON vPerson.PersonID = ProgramParticipation.PersonID
+LEFT JOIN ProgramTerm
+	ON ProgramTerm.ProgramTermID = ProgramParticipation.ProgramTermID
+LEFT JOIN vCurrentStatusLoc
+	on vCurrentStatusLoc.PersonID = ProgramParticipation.PersonID AND vCurrentStatusLoc.ProgramTermID = ProgramParticipation.ProgramTermID AND vCurrentStatusLoc.ProgramID = ProgramTerm.ProgramID
+LEFT JOIN vFamilyPhone as PrimaryPhone
+	ON PrimaryPhone.FamilyID = vFamily.FamilyID AND
+		PrimaryPhone.PhoneRank <= 1
+LEFT JOIN vFamilyPhone as SecondaryPhone
+	ON SecondaryPhone.FamilyID = vFamily.FamilyID AND
+		SecondaryPhone.PhoneRank = 2
+WHERE YEAR(ProgramTerm.BeginDate) = 2017 
+	AND FamilyMembership.FamilyID = '73a0ac56-6a33-480a-ae93-5bf444d18ef2'
