6c0f626a56586b6c015fbb44ed362c25fbeff4ab
Dave Boyce <daveb@501commons.org>
2018-11-08 15:57:36 -0800

    updates

---
+++ Clients/OPPCO/Queries/query-HousingPool-Contact-Sandbox.sql
@@ -0,0 +1,26 @@
+select
+    Household.timestamp_created
+    ,"key_foreign_waitlist_id"
+    ,"Client_Name_First"
+    ,"Client_Name_Last"
+    ,"Client_DOB"
+    ,"Client_Relationship_to_Household"
+    ,"Client_Gender"
+    ,"Client_Ethnicity"
+    ,"Client_Race"
+    ,"Client_Language"
+    ,"Client_Pregnancy_Due_Date"
+    ,"Client_Custody_Status"
+    ,"Client_Intimate_Partner_Violence"
+    ,"Client_Medically_Fragile"
+    ,"Client_Veteran"
+    ,"Client_Disabled"
+    ,"Client_Age"
+    ,"Client_MH_Disability"
+    ,"Client_MH_Services"
+from
+    Household
+INNER JOIN Waitlist
+       ON Household.key_foreign_waitlist_id = Waitlist.key_primary_waitlist_id
+where
+        intake_date >= '10/01/2017' and intake_date <= '10/30/2017'
---
+++ Clients/OPPCO/Queries/query-HousingPool-ServiceHousehold-Sandbox.sql
@@ -0,0 +1,23 @@
+select 
+    timestamp_created
+    ,key_primary_waitlist_id
+    ,intake_date
+    ,intake_case_manager
+    ,outcome_status
+    ,outcome_outcome
+    ,outcome_date_inactive
+    ,outcome_case_manager
+    ,gen_unit_size
+    ,income_monthly
+    ,income_source
+    ,intake_client_phone_numbers
+    ,intake_client_mailing_address
+    ,intake_client_city
+    ,intake_client_state
+    ,intake_client_zip
+    ,gen_family_size
+    ,gen_family_type
+from
+    Waitlist
+where
+    Waitlist.intake_date >= '10/01/2017' and Waitlist.intake_date <= '10/30/2017'
